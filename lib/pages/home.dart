ebb3daa7fd26f4fd43008215b48317961452a97e
Keller <pasckell01@gmail.com>
2026-05-05 19:08:50 +0200

    ListTiles mit Pokemon finalisiert

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,6 +17,60 @@ Future<List<dynamic>> fetchPokemon() async {
   }
 }
 
+Color typeColor(String type) {
+  switch (type.toLowerCase()) {
+    case 'feuer':
+      return Colors.red;
+    case 'wasser':
+      return Colors.blue;
+    case 'pflanze':
+      return Colors.green;
+    case 'elektro':
+      return Colors.amber;
+    case 'eis':
+      return Colors.cyanAccent;
+    case 'kampf':
+      return Colors.orange;
+    case 'gift':
+      return Colors.purple;
+    case 'boden':
+      return Colors.brown;
+    case 'flug':
+      return Colors.lightBlue;
+    case 'psycho':
+      return Colors.pink;
+    case 'käfer':
+      return Colors.lightGreen;
+    case 'gestein':
+      return Colors.grey;
+    case 'geist':
+      return Colors.deepPurple;
+    case 'drache':
+      return Colors.indigo;
+    case 'unlicht':
+      return Colors.black87;
+    case 'stahl':
+      return Colors.blueGrey;
+    case 'fee':
+      return Colors.pinkAccent;
+    default:
+      return Colors.brown.shade300;
+  }
+}
+
+LinearGradient typeGradient(String t1, String? t2) {
+  return LinearGradient(
+    colors: [
+      typeColor(t1),
+      typeColor(t1),
+      typeColor(t2 ?? t1),
+      Colors.white,
+    ],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -28,7 +82,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Pokédex')),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+            'Pokédex',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.black12,
+      ),
       body: FutureBuilder<List<dynamic>>(
         future: fetchPokemon(),
         builder: (context, snapshot) {
@@ -46,13 +109,39 @@ class _HomeState extends State<Home> {
             itemCount: data.length,
             itemBuilder: (context, index) {
               final pokemon = data[index];
-
-              return ListTile(
-                title: Text(pokemon['name']),
-                subtitle: Text("#${pokemon['nummer']}"),
-                onTap: () {
-                  print(pokemon['url']);
-                },
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  gradient: typeGradient(
+                      pokemon['type1'],
+                      pokemon['type2']
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          pokemon['name'],
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Text(
+                          "#${pokemon['nummer']}",
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                      ],
+                    ),
+                    Image.network(
+                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon['nummer']}.png",
+                      width: 70,
+                      height: 70,
+                    ),
+                  ],
+                ),
               );
             },
           );
